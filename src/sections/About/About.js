d05424160f39dd2a36e5379cdd3491d05b5323fd
Peter Baggetta <petebaggetta@gmail.com>
2023-09-11 19:55:22 -0400

    Fixed smaller phone view

--- src/sections/About/About.js
+++ src/sections/About/About.js
@@ -10,7 +10,7 @@ export function About() {
                         <div class="about-info">    
                             <p>I am a software developer who enjoys full stack programming as well as foundational computer mechanisms. 
                             I completed my degree as a <span class="aboutme-important">Computer Engineer</span> from the 
-                            <span class="aboutme-important">University of Guelph</span> and have gained an extensive 
+                            <span class="aboutme-important"> University of Guelph</span> and have gained an extensive 
                             amount of experience from projects and past jobs throughout that time. I hope to pursue my P.Eng. licence in a 
                             challenging environment where I can demonstrate my collaborative skills by working in teams, while also 
                             providing applicable design skills and programming knowledge to work towards innovative solutions and expand my knowledge!
